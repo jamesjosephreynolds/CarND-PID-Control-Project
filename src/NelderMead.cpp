e6003e77960d115c5e17372ca5909be507460a5f
James Reynolds <jamesjosephreynolds@gmail.com>
2017-05-12 14:56:41 -0400

    Update NelderMead.cpp

--- src/NelderMead.cpp
+++ src/NelderMead.cpp
@@ -1,79 +1,77 @@
 #include "NelderMead.h"
 #include <iostream>
 using namespace std;
+#includ "PID.h"
 
-class NelderMead // Nelder Mead geometric optimization
-{
-  public:
-    int n;                     // number of datapoints for each run, per vertex
-    std::vector<PID> vertex_pid;     // PID parameters for each vertex of optimizer
-    std::vector<double> vertex_cost; // cost value for vertex update
-    int iter;                  // number of iterations to optimize
-    int i;                     // current vertex
-    double a, g, r, s; // alpha, gamma, rho, sigma coefficients
+NelderMead::NelderMead(int N) { //constructor
+  // Vertex update coefficients
+  c_a = 1.0;
+  c_g = 2.0;
+  c_r = 0.5;
+  c_s = 0.5;
+
+  // Vertices and vertex costs
+  vertex_pid.reserve(N);
+  vertex_cost.reserve(N);
   
-    NelderMead(N) : 
-    {
-      a = 1.0;
-      g = 2.0;
-      r = 0.5;
-      s = 0.5;
-      vertex_pid.reserve(N);
-      vertex_cost.reserve(N);
-    } // constructor
+  // Optimization hyperparameters
+  num_pts = 1000;
+  num_iter = 50;
+  cur_vertex = 0;
+}
+
+NelderMead::~NelderMead() {}
   
-    void clearCosts() {
-      // reset the costs for vertices
-      for (int i = 0; i < NUM_VERTICES; ++i) {
-        vertex_cost[i] = 0.0f;    
-      }
-    }
+void NelderMead::clearCosts() {
+  // reset the costs for vertices
+  for (int i = 0; i < NUM_VERTICES; ++i) {
+    vertex_cost[i] = 0.0f;    
+  }
+}
   
-    PID getCentroid() {
-      // return the centroid PID value of the n-1 best vertices
-      PID pid;
+PID NelderMead::getCentroid() {
+  // return the centroid PID value of the n-1 best vertices
+  PID pid;
     
-      // find the worst vertex for centroid exclusion
-      int worst = -1;
-      double cost = -1.0f;
-      for (int i = 0; i < NUM_VERTICES; ++i) {
-        if (vertex_cost[i] > cost) {
-          worst = i;
-          cost = vertex_cost[i];
-        }
-      }
-    
-      double Kp_tmp = 0.0f;
-      double Ki_tmp = 0.0f;
-      double Kd_tmp = 0.0f;
-      
-      // calculate the centroid
-      for (int i = 0; i < NUM_VERTICES; ++i) {
-        if (i != worst) {
-          Kp_tmp += vertex_pid[i].Kp_;
-          Ki_tmp += vertex_pid[i].Ki_;
-          Kd_tmp += vertex_pid[i].Kd_;
-        }
-      }
-      Kp_tmp /= double(NUM_VERTICES - 1);
-      Ki_tmp /= double(NUM_VERTICES - 1);
-      Kd_tmp /= double(NUM_VERTICES - 1);
-    
-      // return the centroid
-      pid.Init(Kp_tmp, Ki_tmp, Kd_tmp);
-      pid.printPID();
-      return pid
+  // find the worst vertex for centroid exclusion
+  int worst = -1;
+  double cost = -1.0f;
+  for (int i = 0; i < NUM_VERTICES; ++i) {
+    if (vertex_cost[i] > cost) {
+      worst = i;
+      cost = vertex_cost[i];
     }
-  
-    void printOptimizer() {
-      // print optimizer parameters for debugging and datalogging
-      std::cout << "Number of iterations: " << iter << std::endl;
-      std::cout << "Number of datapoints to eval: " << n << std::endl;
-      for (int i = 0; i < NUM_VERTICES; ++ i) {
-      std::cout << "Vertex " << i << ":" << std::endl;
-      vertex_pid[i].printPID();
-      }
-      std::cout << "Parameters (a, g, r, s): << {a, g, r, s} << std::endl;
+  }
+    
+  double Kp_tmp = 0.0f;
+  double Ki_tmp = 0.0f;
+  double Kd_tmp = 0.0f;
+   
+  // calculate the centroid
+  for (int i = 0; i < NUM_VERTICES; ++i) {
+    if (i != worst) {
+      Kp_tmp += vertex_pid[i].Kp_;
+      Ki_tmp += vertex_pid[i].Ki_;
+      Kd_tmp += vertex_pid[i].Kd_;
     }
+  }
+  Kp_tmp /= double(NUM_VERTICES - 1);
+  Ki_tmp /= double(NUM_VERTICES - 1);
+  Kd_tmp /= double(NUM_VERTICES - 1);
+    
+  // return the centroid
+  pid.Init(Kp_tmp, Ki_tmp, Kd_tmp);
+  pid.printPID();
+  return pid
+}
   
-};
+void NelderMead::printOptimizer() {
+  // print optimizer parameters for debugging and datalogging
+  std::cout << "Number of iterations: " << iter << std::endl;
+  std::cout << "Number of datapoints to eval: " << n << std::endl;
+  for (int i = 0; i < NUM_VERTICES; ++ i) {
+  std::cout << "Vertex " << i << ":" << std::endl;
+  vertex_pid[i].printPID();
+  }
+  std::cout << "Parameters (a, g, r, s): << {a, g, r, s} << std::endl;
+}
