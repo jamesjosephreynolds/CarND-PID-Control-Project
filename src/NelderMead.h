0425e6bd41d19e32b9bd2e62feb301670624b40d
James Reynolds <jamesjosephreynolds@gmail.com>
2017-05-12 15:03:00 -0400

    Update NelderMead.h

--- src/NelderMead.h
+++ src/NelderMead.h
@@ -1,7 +1,17 @@
 #ifndef NELDERMEAD_H
 #define NELDERMEAD_H
 
-class NelderMead // Nelder Mead geometric optimization
+/*
+ * Nelder-Mead is a geometric optimizer that is derivative free
+ * Unlike Twiddle, Nelder-Mead modifies multiple parameters per run,
+ * and the update steps are a function of the "badness" of the worst
+ * performing PID parameters, relative to the remaining parameters.
+ * 
+ * For three parameters (Kp, Ki, Kd), four combinations of gains will
+ * form the vertices of a tetrahedron in R3, and one vertex will be
+ * moved based on evaluation of all four.
+ */
+class NelderMead
 {
   public:
     int num_pts;                     // number of datapoints for each run, per vertex
