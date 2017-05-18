9c2c6f4312bd91e7b6de1d8b850d5cd3922457b9
Jim Reynolds <jamesjosephreynolds@gmail.com>
2017-05-18 13:34:26 -0400

    ready to submit

--- src/main.cpp
+++ src/main.cpp
@@ -91,7 +91,7 @@ int main()
           double throttle_value = feedforward + pid_sp.TotalError();
           if (throttle_value > 0.5f) {
             throttle_value = 0.5f;
-          } else if (steer_value < -0.2f) {
+          } else if (throttle_value < -0.2f) {
             // Inhibit excessive braking
             throttle_value = -0.2f;
           }
